1cfe5307d48b3093b9e34eac544c058b8fb29b53
Andrea Iannì <andrea.ianni@avbo.it>
2023-11-07 11:01:30 +0100

    Aggiunto evento al carimento della pagina all'interno dello script

---
+++ script.js
@@ -0,0 +1,7 @@
+//Aggiunge un evento che si attiva quando il DOM viene caricato
+//	e richiama la funzione onAllLoaded
+document.addEventListener("DOMContentLoaded", onAllLoaded);
+
+//Definizione di una funzione che prende in input un argomento
+function onAllLoaded(event) {
+}
