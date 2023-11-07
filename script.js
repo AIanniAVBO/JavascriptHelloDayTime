47baaf673e11290cd86170912f95ed036d3eb5d9
Andrea Iannì <andrea.ianni@avbo.it>
2023-11-07 11:03:17 +0100

    Aggiunto un evento al tasto helloButton che richiama una funzione

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ document.addEventListener("DOMContentLoaded", onAllLoaded);
 
 //Definizione di una funzione che prende in input un argomento
 function onAllLoaded(event) {
+	
+	//Ricerca il tasto helloButton e associa al click la funzione helloButtonClicked
+	document.getElementById("helloButton").addEventListener("click", helloButtonClicked);
+}
+
+//Definizione di una funzione che prende in input un argomento
+function helloButtonClicked(event) {
 }
